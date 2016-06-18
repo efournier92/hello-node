9249808030f6277ec275ffb74af4140b61187dd0
Misigno <efournier92@gmail.com>
2016-06-18 18:41:10 -0400

    add admin route

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ var express = require('express');
 var app     = express();
 var path    = require('path');
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { 
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(1337);
 console.log('Check out port 1337');
+
+var adminRouter = express.Router();
+
+adminRouter.get('/', function(req, res) {
+  res.send('I am the dashboard');              
+});
+
+adminRouter.get('/users', function(req, res) {
+  res.send('I show all the users!');
+});
+
+adminRouter.get('/posts', function(req, res) {
+  res.send('I show all the posts!');
+});
+
+app.use('/admin', adminRouter);
